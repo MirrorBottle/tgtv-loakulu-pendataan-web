489d103cd765c0e693b2ccd536d66e104757fed7
MirrorBottle <setiawanbayu66152@gmail.com>
2023-07-01 14:19:15 +0800

    feat: add database

--- storage/database/inventory_categories.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO inventory_categories (id, name) VALUES (1, 'Alat Lainnya');
-INSERT INTO inventory_categories (id, name) VALUES (2, 'Alat Makan');
-INSERT INTO inventory_categories (id, name) VALUES (3, 'Bahan Masak');
-INSERT INTO inventory_categories (id, name) VALUES (4, 'Minuman');
-INSERT INTO inventory_categories (id, name) VALUES (5, 'Plastik');
